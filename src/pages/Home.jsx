62149a01352f99f5e179c73464c5c249af854dd5
hcy <1306574172@qq.com>
2025-03-16 18:00:50 +0800

    样式调整

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../components/Hero/Hero";
+import Services from "../components/Services/Services";
+import Footer from "../components/Footer/Footer";
+import FetchShowcase from "../components/FetchShowcase/FetchShowcase";
+
+const Home = () => {
+  return (
+    <div className="overflow-x-hidden bg-gray-50">
+      <Hero />
+      <FetchShowcase />
+      <Services />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
